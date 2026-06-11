6dfb7e2e5007978c90d7a43808446cfee6aba4c6
vaibhav <vaibhavsinghrajput456@gmail.com>
2026-06-11 14:10:22 +0530

    upda

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+console.log("Build started!");
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
