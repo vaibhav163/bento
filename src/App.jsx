b70cc3727e5b64d1a50af477ceb2cdaa59de1de0
vaibhav <vaibhavsinghrajput456@gmail.com>
2026-06-25 15:05:36 +0530

    Add Vercel rewrites

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
-import Bent from './components/bent'
-import {Routes, Route,} from 'react-router-dom'
-import Crys from './components/Gaurchrysalis2'
+// import React from 'react'
+// import Bent from './components/bent'
+// import {Routes, Route,} from 'react-router-dom'
+// import Crys from './components/Gaurchrysalis2'
+// function App() {
+//   return (
+//     <>
+//       <Routes>
+//         <Route path="" element={<Bent/>} />
+//         <Route path="/Chrysalis" element={<Crys />} />
+//         <Route path="/bento" element={<Bent />} />
+//       </Routes>
+//     </>
+//   )
+// }
+
+// export default App
+import React from "react";
+import Bent from "./components/bent";
+import Crys from "./components/Gaurchrysalis2";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="" element={<Bent/>} />
-        <Route path="/Chrysalis" element={<Crys />} />
-        <Route path="/bento" element={<Bent />} />
-      </Routes>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Bent />} />
+      <Route path="/Chrysalis" element={<Crys />} />
+      <Route path="/bento" element={<Bent />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
